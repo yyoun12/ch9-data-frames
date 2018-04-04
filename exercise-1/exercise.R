e32dc4f8ada17eaee8cde2012fed2b58962cf1ad
Yu-Won Youn <Yu-Won@D-108-179-179-250.dhcp4.washington.edu>
2018-04-04 16:58:02 -0700

    ch9 ex1 180404

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,14 +2,14 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+points_scored <- c(9, 12, 27, 46)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+points_given <- c(17, 9, 33, 18)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- data.frame(points_scored, points_given)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
